68e0000cbff952f29ea129b63222808948c79e4c
RajR1996 <raj_r96@hotmail.co.uk>
2019-03-16 00:35:09 +0000

    Completed ADM Assignment 2

--- Old/includes/footer.php
+++
@@ -1,7 +0,0 @@
-	<!-- Footer -->
-  <!-- <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; ADM 2019</p>
-    </div>
-    <!-- /.container
-  </footer> --> -->
